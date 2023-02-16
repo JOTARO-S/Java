1b5fe59d223679d1ddd32cd2c342f549a374506e
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-02-16 02:39:00 +0000

    Chapter4

---
+++ 4章_変数を学ぼう/Chapter04.java
@@ -0,0 +1,14 @@
+public class Chapter04 {
+    
+    public static void main(String[] args) {
+        
+        final int Lucky_num = 777;
+        
+        String str = "Jotaro_s";
+        System.out.println(str);
+        
+        str = "Happy birthday!";
+        System.out.println(str);
+        System.out.println(Lucky_num);
+    }
+}
